5ebdec350ca6bd04262f2fae23d4d1346157e23e
Tien Le T.C <tien.le2@asiantech.vn>
2019-09-29 14:06:34 +0700

    Change width and padding

--- todo-vue-pwa/src/components/features/Form.vue
+++ todo-vue-pwa/src/components/features/Form.vue
@@ -3,8 +3,8 @@
     <div class="container text-between">
       <form>
         <div class="flex-centered">
-          <input type="text" class="input-todo" placeholder="Enter something">
-          <button class="btn btn-circle">+</button>
+          <input type="text" class="input-todo" placeholder="What needs to be done?">
+          <button class="btn btn-circle"><i class="fa fa-plus"></i></button>
         </div>
       </form>
     </div>
