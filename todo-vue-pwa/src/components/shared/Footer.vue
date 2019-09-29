bd5027740e264c64f4574d6b3212261b03c512df
at-manhle <manh.le@asiantech.vn>
2019-09-29 10:16:49 +0700

    add some style for footer

--- todo-vue-pwa/src/components/shared/Footer.vue
+++ todo-vue-pwa/src/components/shared/Footer.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="footer">
-    Footer
+    <ul class="filter-list flex">
+      <li class="filter-item">
+        <button class="btn warning-btn">123</button>
+      </li>
+      <li class="filter-item">
+        <button class="btn success-btn">123213</button>
+      </li>
+      <li class="filter-item">
+        <button class="btn danger-btn">4124</button>
+      </li>
+      <li class="filter-item">
+        <button class="btn primary-btn">1242</button>
+      </li>
+    </ul>
   </div>
 </template>
 
