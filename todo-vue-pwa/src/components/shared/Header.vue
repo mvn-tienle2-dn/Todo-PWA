52c57292ec7d9c374d58a0aa76ec673c3bc5a589
Tien Le T.C <tien.le2@asiantech.vn>
2019-09-29 10:51:58 +0700

    Change some style for header

--- todo-vue-pwa/src/components/shared/Header.vue
+++ todo-vue-pwa/src/components/shared/Header.vue
@@ -2,14 +2,15 @@
   <div class="page-header">
     <div class="container flex-between">
       <div class="header-left">
+        <h6 class="app-title txt-upper">todos</h6>
       </div>
       <div class="header-center">
         <a href="/" class="page-logo flex-centered">
           <img src="../../assets/img/logo.png" alt="Logo">
-          <span class="app-title ml-1">Todo App</span>
         </a>
       </div>
       <div class="header-right">
+        <img src="../../assets/img/user-icon.png" alt="User">
       </div>
     </div>
   </div>
