c86a36443489a44aa9a9dd55d82c2f43ad35af32
Tien Le T.C <tien.le2@asiantech.vn>
2019-09-29 10:06:23 +0700

    Making UI for header

--- todo-vue-pwa/src/components/Todo.vue
+++ todo-vue-pwa/src/components/Todo.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="page-todo">
-    To do app
     <Header/>
     <TodoForm/>
     <TodoList/>
--- todo-vue-pwa/src/components/shared/Header.vue
+++ todo-vue-pwa/src/components/shared/Header.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="header">
-    Header
+  <div class="page-header">
+    <div class="container flex-between">
+      <div class="header-left">
+      </div>
+      <div class="header-center">
+        <a href="/" class="page-logo flex-centered">
+          <img src="../../assets/img/logo.png" alt="Logo">
+          <span class="app-title ml-1">Todo App</span>
+        </a>
+      </div>
+      <div class="header-right">
+      </div>
+    </div>
   </div>
 </template>
 
