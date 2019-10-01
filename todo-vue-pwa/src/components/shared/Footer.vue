550890abfdef47b622ad9a5a0894731d7c6ac7e3
at-manhle <manh.le@asiantech.vn>
2019-10-01 22:55:59 +0700

    rename btn convention

--- todo-vue-pwa/src/components/shared/Footer.vue
+++ todo-vue-pwa/src/components/shared/Footer.vue
@@ -2,16 +2,16 @@
   <div class="page-footer">
     <ul class="filter-list flex flex-between flex-vertical-center flex-row flex-wrap">
       <li class="filter-item">
-        <button class="btn primary-btn btn-filter btn-none-border">All Tasks</button>
+        <button class="btn btn-primary btn-filter btn-none-border">All Tasks</button>
       </li>
       <li class="filter-item">
-        <button class="btn warning-btn btn-filter btn-none-border">Actice</button>
+        <button class="btn btn-warning btn-filter btn-none-border">Actice</button>
       </li>
       <li class="filter-item">
-        <button class="btn success-btn btn-filter btn-none-border">Completed</button>
+        <button class="btn btn-success btn-filter btn-none-border">Completed</button>
       </li>
       <li class="filter-item">
-        <button class="btn danger-btn btn-filter btn-none-border">Clear completed</button>
+        <button class="btn btn-danger btn-filter btn-none-border">Clear completed</button>
       </li>
     </ul>
   </div>
