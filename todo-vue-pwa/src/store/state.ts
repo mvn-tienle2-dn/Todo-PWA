57e082eab918499bda4c0d2e380436e5ad656809
Tien Le T.C <tien.le2@asiantech.vn>
2019-10-02 08:35:01 +0700

    Change filter

--- todo-vue-pwa/src/store/state.ts
+++ todo-vue-pwa/src/store/state.ts
@@ -17,56 +17,6 @@ export default {
       content: 'Implement logic',
       status: false,
     },
-    {
-      id: '1',
-      content: 'Implement logic',
-      status: false,
-    },
-    {
-      id: '2',
-      content: 'Implement logic',
-      status: false,
-    },
-    {
-      id: '3',
-      content: 'Implement logic',
-      status: false,
-    },
-    {
-      id: '4',
-      content: 'Implement logic',
-      status: false,
-    },
-    {
-      id: '5',
-      content: 'Implement logic',
-      status: false,
-    },
-    {
-      id: '6',
-      content: 'Implement logic',
-      status: false,
-    },
-    {
-      id: '7',
-      content: 'Implement logic',
-      status: false,
-    },
-    {
-      id: '8',
-      content: 'Implement logic',
-      status: false,
-    },
-    {
-      id: '9',
-      content: 'Implement logic',
-      status: false,
-    },
-    {
-      id: '10',
-      content: 'Implement logic 123',
-      status: false,
-    },
   ],
   filter: null,
 } as State;
