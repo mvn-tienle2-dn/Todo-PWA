f56d5be1ccc24d56682c333db71d7d805b9f4944
Tien Le T.C <tien.le2@asiantech.vn>
2019-10-03 10:16:36 +0700

    Add transition

--- todo-vue-pwa/src/components/features/ListTodo.vue
+++ todo-vue-pwa/src/components/features/ListTodo.vue
@@ -2,15 +2,19 @@
   <div class="page-list-todo">
     <div v-if="todoList.length">
       <h6 class="text-centered total">{{todoList.length}} item(s) left</h6>
-      <ul v-if="todoList.length" class="list-todo">
-        <li v-for="(todo, idx) in todoList" :key="idx" class="todo-item">
-          <div class="checkbox-section">
-            <input class="checkbox-input" type="checkbox" :checked="todo.status === 'done' ? true : false" id="status">
-            <label class="checkbox-label" v-bind:class="todo.status === 'done' ? 'done' : ''" for="status">{{todo.content}}</label>
-          </div>
-          <button class="item-btn"><i class="fa fa-times"></i></button>
-        </li>
-      </ul>
+      <transition name="fade">
+        <ul v-if="todoList.length" class="list-todo">
+          <transition-group name="fade">
+            <li v-for="(todo, idx) in todoList" :key="idx" class="todo-item">
+              <div class="checkbox-section">
+                <input class="checkbox-input" type="checkbox" :checked="todo.status === 'done' ? true : false" id="status">
+                <label class="checkbox-label" v-bind:class="todo.status === 'done' ? 'done' : ''" for="status">{{todo.content}}</label>
+              </div>
+              <button class="item-btn"><i class="fa fa-times"></i></button>
+            </li>
+          </transition-group>
+        </ul>
+      </transition>
     </div>
     <div class="text-centered no-task" v-else>
       <i class="fa fa-tasks"></i>
