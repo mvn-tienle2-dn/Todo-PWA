9aa0e90acfc531532a4bbf53ddb625b9ac8da00a
Tien Le T.C <tien.le2@asiantech.vn>
2019-10-03 17:23:54 +0700

    Update footer

--- todo-vue-pwa/src/components/shared/Footer.vue
+++ todo-vue-pwa/src/components/shared/Footer.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="page-footer">
-    <ul class="filter-list flex flex-between flex-vertical-center flex-row flex-wrap">
-      <li class="filter-item">
-        <button class="btn btn-primary btn-filter btn-none-border" :disabled="!typeFilter" @click="change(null)">All Tasks</button>
-      </li>
-      <li class="filter-item">
-        <button class="btn btn-warning btn-filter btn-none-border" :disabled="typeFilter === 'notdone'" @click="change('notdone')">Actice</button>
-      </li>
-      <li class="filter-item">
-        <button class="btn btn-success btn-filter btn-none-border" :disabled="typeFilter === 'done'" @click="change('done')">Completed</button>
-      </li>
-      <li class="filter-item">
-        <button class="btn btn-danger btn-filter btn-none-border">Clear completed</button>
-      </li>
-    </ul>
+    <div class="container">
+      <ul class="filter-list flex flex-between flex-vertical-center flex-row flex-wrap">
+        <li class="filter-item">
+          <button class="btn btn-primary btn-filter btn-none-border" :disabled="!typeFilter" @click="change(null)">All Tasks</button>
+        </li>
+        <li class="filter-item">
+          <button class="btn btn-warning btn-filter btn-none-border" :disabled="typeFilter === 'notdone'" @click="change('notdone')">Actice</button>
+        </li>
+        <li class="filter-item">
+          <button class="btn btn-success btn-filter btn-none-border" :disabled="typeFilter === 'done'" @click="change('done')">Completed</button>
+        </li>
+        <li class="filter-item">
+          <button class="btn btn-danger btn-filter btn-none-border">Clear completed</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script lang="ts">
