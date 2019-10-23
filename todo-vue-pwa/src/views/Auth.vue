dee075e31de6bcf9157369ceabb6b3375dfdc6ac
Tien Le T.C <tien.le2@asiantech.vn>
2019-10-23 17:17:57 +0700

    Making UI for auth

--- todo-vue-pwa/src/views/Auth.vue
+++ todo-vue-pwa/src/views/Auth.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="page-auth">
-    Authentication
+    <div class="site-wrapper">
+      <div class="form-wrapper">
+        <div class="box">
+          <input type="radio" id="signInTitle" class="hiddenInput" name="formTitle" checked>
+          <label class="tab-item" for="signInTitle">Sign In</label>
+          <input type="radio" id="signUpTitle" class="hiddenInput" name="formTitle">
+          <label class="tab-item active" for="signUpTitle">Sign Up</label>
+          <div class="clearfix"></div>
+          <div class="singIn">
+            Sign In
+          </div>
+          <div class="singUp">
+            Sign Up
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-
   export default ({
     name: 'Auth',
   });
