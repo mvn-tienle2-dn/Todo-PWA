e612cbcafafeddc8e33d1af95db954b5769c8bcf
Tien Le T.C <tien.le2@asiantech.vn>
2019-10-29 10:42:51 +0700

    Add loading when processing

--- todo-vue-pwa/src/components/features/ListTodo.vue
+++ todo-vue-pwa/src/components/features/ListTodo.vue
@@ -1,24 +1,29 @@
 <template>
-  <div v-if="!isProcessing" class="page-list-todo">
-    <div v-if="todoList.length">
-      <h6 class="text-centered total">You have {{todoList.length}} task(s) here.</h6>
-      <transition name="fade">
-        <ul v-if="todoList.length" class="list-todo">
-          <transition-group name="fade">
-            <li v-for="(todo, idx) in todoList" :key="idx" class="todo-item">
-              <div class="checkbox-section">
-                <input class="checkbox-input" type="checkbox" @click="handleUpdateStatus(todo.id)" :checked="todo.status === 'done' ? true : false" :id="todo.id">
-                <label class="checkbox-label" v-bind:class="todo.status === 'done' ? 'done' : ''" :for="todo.id">{{todo.content}}</label>
-              </div>
-              <button class="item-btn" @click="handleRemoveTodo(todo.id)"><i class="fa fa-trash-alt"></i></button>
-            </li>
-          </transition-group>
-        </ul>
-      </transition>
+  <div class="page-list-todo">
+    <div v-if="isProcessing">
+      <Loading/>
     </div>
-    <div class="text-centered no-task" v-else>
-      <i class="fa fa-tasks"></i>
-      <h6 class="no-task-msg">You don't have any task.</h6>
+    <div v-else>
+      <div v-if="todoList.length">
+        <h6 class="text-centered total">You have {{todoList.length}} task(s) here.</h6>
+        <transition name="fade">
+          <ul v-if="todoList.length" class="list-todo">
+            <transition-group name="fade">
+              <li v-for="(todo, idx) in todoList" :key="idx" class="todo-item">
+                <div class="checkbox-section">
+                  <input class="checkbox-input" type="checkbox" @click="handleUpdateStatus(todo.id)" :checked="todo.status === 'done' ? true : false" :id="todo.id">
+                  <label class="checkbox-label" v-bind:class="todo.status === 'done' ? 'done' : ''" :for="todo.id">{{todo.content}}</label>
+                </div>
+                <button class="item-btn" @click="handleRemoveTodo(todo.id)"><i class="fa fa-trash-alt"></i></button>
+              </li>
+            </transition-group>
+          </ul>
+        </transition>
+      </div>
+      <div class="text-centered no-task" v-else>
+        <i class="fa fa-tasks"></i>
+        <h6 class="no-task-msg">You don't have any task.</h6>
+      </div>
     </div>
   </div>
 </template>
@@ -26,9 +31,13 @@
 <script lang="ts">
   import { mapGetters } from 'vuex';
   import { mapActions } from 'vuex';
+  import Loading from '../shared/Loading.vue';
 
   export default ({
     name: 'TodoList',
+    components: {
+      Loading,
+    },
     computed: {
       ...mapGetters(['todoList']),
     },
@@ -48,7 +57,9 @@
     },
     mounted() {
       this.setDataToState();
-      this.isProcessing = false;
-    }
+      setTimeout(() => {
+        this.isProcessing = false;
+      }, 1000);
+    },
   });
 </script>
---
+++ todo-vue-pwa/src/components/shared/Loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="loading-bro">
+    <svg id="load" x="0px" y="0px" viewBox="0 0 150 150">
+      <circle id="loading-inner" cx="75" cy="75" r="60"/>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+
+  export default ({
+    name: 'Loading'
+  });
+</script>
