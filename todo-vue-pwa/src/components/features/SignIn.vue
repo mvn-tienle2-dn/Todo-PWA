ae798fa7de7c3c16240cfaa395fbae0381640caf
Tien Le T.C <tien.le2@asiantech.vn>
2019-10-29 11:49:14 +0700

    Add loading

--- todo-vue-pwa/src/components/features/SignIn.vue
+++ todo-vue-pwa/src/components/features/SignIn.vue
@@ -1,35 +1,51 @@
 <template>
-  <form>
-    <input class="input email" id="emailIn" type="email" placeholder="Email" v-model="email">
-    <input class="input password" id="passwordIn" type="password" placeholder="Password" v-model="password">
-    <button type="button" class="btn btn-primary" @click="login()">Submit</button>
-    <div class="action">
-      <button type="button" class="btn btn-fb" @click="signinWithFB()"><i class="fab fa-facebook-f"></i></button>
-      <button type="button" class="btn btn-google" @click="signinWithGoogle()"><i class="fab fa-google"></i></button>
+  <div class="login">
+    <div v-if="isProcessing">
+      <Loading/>
     </div>
-  </form>
+    <div v-else>
+      <form>
+        <input class="input email" id="emailIn" type="email" placeholder="Email" v-model="email">
+        <input class="input password" id="passwordIn" type="password" placeholder="Password" v-model="password">
+        <button type="button" class="btn btn-primary" @click="login()">Submit</button>
+        <div class="action">
+          <button type="button" class="btn btn-fb" @click="signinWithFB()"><i class="fab fa-facebook-f"></i></button>
+          <button type="button" class="btn btn-google" @click="signinWithGoogle()"><i class="fab fa-google"></i></button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
   import firebase from 'firebase';
   import { mapActions } from 'vuex';
+  import Loading from '../shared/Loading.vue'
 
   export default ({
     name: 'Login',
+    components: {
+      Loading,
+    },
     data() {
       return {
         email: null,
         password: null,
+        isProcessing: false,
       };
     },
     methods: {
       ...mapActions(['signin', 'signinWithGoogle', 'signinWithFB']),
       login() {
+        this.isProcessing = true;
         const payload = {
           email: this.email,
           password: this.password,
         };
         this.signin(payload);
+        setTimeout(() => {
+          this.isProcessing = false;
+        }, 2000);
       },
     },
   });
